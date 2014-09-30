fb2ecfbb9e3b249b1569a7eaa39d41982b790c95
Mike Cote <mcote@i-sight.com>
2014-09-30 08:57:37 -0400

    Wait for all attachments to be uploaded before calling the mail event of mail-reader. This allows to save ids of attachments within the email object.

--- mail-reader.js
+++ mail-reader.js
@@ -2,6 +2,7 @@
 var MailParser = require('mailparser').MailParser
 var fs = require('fs')
 var uuid = require('uuid')
+var _ = require('lodash');
 
 var pluginName = 'mail-reader'
 
@@ -30,21 +31,46 @@ module.exports = function(options) {
     done()
   })
 
-  function redirectAttachmentStreamToSeneca(attachment) {
-    seneca.act({role: pluginName, cmd: 'attachment', attachment: attachment})
-  }
-
   seneca.add({role: pluginName, cmd: 'connection'}, function(args, done) {
     done()
   })
 
   seneca.add({role: pluginName, cmd: 'writeChunk'}, function(args, done) {
     if(!args.connection.id) {
+      // Keep count of how many attachments, this will allow us to wait for all of the attachments
+      // to be uploaded before triggering the mail event.
+      var attachmentCount = 0;
       args.connection.id = uuid.v4()
       args.connection.parser = new MailParser({ streamAttachments: true })
-      args.connection.parser.on("attachment", redirectAttachmentStreamToSeneca)
+      // Create function that will be called after each attachment is uploaded. For now it will
+      // decrease the count of attachments, but once the 'end' event is triggered, this function
+      // will be replaced with a _.after. Using _.after will wait for function to be called
+      // (attachmentCount) times before moving along.
+      var doneUploadingAttachment = function () {
+        attachmentCount--;
+      };
+      args.connection.parser.on('attachment', function (attachment) {
+        attachmentCount++;
+        // Redirect attachment to seneca
+        seneca.act({
+          role: pluginName,
+          cmd: 'attachment',
+          connection: args.connection,
+          attachment: attachment
+        }, function () {
+          // Mark this attachment as uploaded
+          doneUploadingAttachment();
+        });
+      });
       args.connection.parser.on("end", function(mail){
-        seneca.act({role: pluginName, cmd: 'mail', mail: mail, connection: args.connection})
+        // Now that we have a count of actual attachments, lets wait for x amount of callbacks
+        // to be called before sending the new mail event. This way each attachment can add info
+        // to the connection and link attachment ids to the mail
+        doneUploadingAttachment = _.after(attachmentCount, function () {
+          seneca.act({role: pluginName, cmd: 'mail', mail: mail, connection: args.connection})
+        });
+        // If there's no attachments, call mail
+        if (attachmentCount === 0) { doneUploadingAttachment(); }
       })
     }
     args.connection.parser.write(args.chunk)
